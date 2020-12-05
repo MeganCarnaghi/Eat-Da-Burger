cbf28d53777ca14497d177369b68d7247d80f2fc
Megan Carnaghi <carnagh5@gmail.com>
2020-12-05 09:16:32 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Required dependencies
 var express = require("express");
 var exphbs = require("express-handlebars");
+var methodOverride = require('method-override');
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -19,6 +20,9 @@ app.use(express.json());
 // Set Handlebars as the default templating engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 // Import the routes and give the server access to them
 var routes = require('./controllers/burgers_controller.js');
