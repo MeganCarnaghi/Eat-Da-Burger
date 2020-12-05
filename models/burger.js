186681c5912f6f50d84a07f07643e7b19d62e934
Megan Carnaghi <carnagh5@gmail.com>
2020-12-05 09:20:58 -0500

    Added selectAll, insertOne and updateOne functions to the burger.js file.

--- models/burger.js
+++ models/burger.js
@@ -2,5 +2,29 @@
 // Import the ORM to implement functions that will interact with the database
 var orm = require('../config/orm.js');
 
-// Export the database functions for the controller
+// Create the burger object
+var burger = {
+    // Select all burger records from the database
+    selectAll: (cb) => {
+      orm.selectAll('burgers', function(res) {
+        cb(res);
+      });
+    },
+  
+    // The variables cols and vals are arrays
+    insertOne: (cols, vals, cb) => {
+      orm.insertOne('burgers', cols, vals, function(res) {
+        cb(res);
+      });
+    },
+  
+    // The objColVals is an object specifying columns as object keys with associated values
+    updateOne: (objColVals, condition, cb) => {
+      orm.updateOne('burgers', objColVals, condition, (res) => {
+        cb(res);
+      });
+    }
+  };
+
+// Export the database functions for the controller (burgers_controller.js)
 module.exports = burger;
