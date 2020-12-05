3bd02dce7271bf9ee480b0bc299d3fdcf85e6bbb
Megan Carnaghi <carnagh5@gmail.com>
2020-12-05 09:31:16 -0500

    Added POST and PUT routes to controller file. Added burger image.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,7 @@ var router = express.Router();
 var burger = require('../models/burger.js');
 
 // Create routes
-// GET route
+// GET route to get all burgers from the database
 router.get('/', (req, res) => {
     burger.selectAll(function(data) {
       var hbsObject = {
@@ -16,9 +16,27 @@ router.get('/', (req, res) => {
     });
   });
 
-// POST route
+// POST route to insert a burger into the database
+router.post('/burgers', (req, res) => {
+    burger.insertOne([
+      'burger_name'
+    ], [
+      req.body.burger_name
+    ], (data) => {
+      res.redirect('/');
+    });
+  });
 
-// PUT route
+// PUT route to change a burger's devoured status to true
+router.put('/burgers/:id', (req, res) => {
+    var condition = 'id = ' + req.params.id;
+  
+    burger.updateOne({
+      devoured: true
+    }, condition, (data) => {
+      res.redirect('/');
+    });
+  });
 
 // Export routes for the server (server.js) to use
 module.exports = router;
