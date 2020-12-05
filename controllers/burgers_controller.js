a87b055cf984bd48b1afda71a3c619d35a43dc4d
Megan Carnaghi <carnagh5@gmail.com>
2020-12-05 09:23:57 -0500

    Began creating routes in burgers_controller.js file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,8 +2,23 @@
 var express = require('express');
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
+// Import the model (burger.js) to use its database functions
 var burger = require('../models/burger.js');
 
-// Export routes for server.js to use.
+// Create routes
+// GET route
+router.get('/', (req, res) => {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      res.render('index', hbsObject);
+    });
+  });
+
+// POST route
+
+// PUT route
+
+// Export routes for the server (server.js) to use
 module.exports = router;
